35ef6be15551f3f86269e996a0fab8c48eb28e11
Belal Khan <probelalkhan@gmail.com>
2018-12-21 11:29:29 +0530

    Read Issue Fixed

--- src/DbHandler.java
+++ src/DbHandler.java
@@ -67,22 +67,22 @@ public class DbHandler {
                     "jdbc:mysql://localhost:3306/zak", "root", "");
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM farmingstat");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM workdiary");
             while (rs.next()) {
 
                 WorkDiary wd = new WorkDiary(
-                        rs.getInt(0),
-                        rs.getString(1),
+                        rs.getInt(1),
                         rs.getString(2),
                         rs.getString(3),
                         rs.getString(4),
-                        rs.getInt(5),
-                        rs.getString(6),
+                        rs.getString(5),
+                        rs.getInt(6),
                         rs.getString(7),
                         rs.getString(8),
                         rs.getString(9),
                         rs.getString(10),
-                        rs.getString(11)
+                        rs.getString(11),
+                        rs.getString(12)
                 );
                 workDiaries.add(wd);
             }
